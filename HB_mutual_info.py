c1f99670c59888f48b5b8bbf061da7d12ed32ba7
sherpaman <losherpa@gmail.com>
2015-07-12 16:43:26 +0200

    Added time-series class with some basic information teory tool and a script to apply them to h-bonds analysis

---
+++ HB_mutual_info.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+
+import numpy as np
+from ts import ts
+import hb
+import xpm
+import gro
+import sys 
+import os
+import math
+import gzip
+import json
+
+#
+# SET THE INTERFACE MODULES
+#
+from argparse import ArgumentParser
+parser = ArgumentParser( description = 'Calculate Mutual Information')
+#
+# INPUT FILES
+#
+parser.add_argument("-f","--gro",dest="file_gro",action="store",type=str,default=None,help="Input GRO File",required=True,metavar="GRO FILE")
+parser.add_argument("-l","--log",dest="file_log",action="store",type=str,default=None,help="Input LOG File",required=True,metavar="LOG FILE")
+parser.add_argument("-x","--xpm",dest="file_xpm",action="store",type=str,default=None,help="Input XPM File",required=True,metavar="XPM FILE")
+parser.add_argument("-m","--mat",dest="file_mut",action="store",type=str,default=None,help="Mutual Info File",required=True,metavar="DAT FILE")
+#
+# OUTPUT FILES
+#
+parser.add_argument("-o","--out",dest="file_out",action="store",type=str,default=None,required=False,help="Output Mutual Info",metavar="DAT FILE")
+#
+# VAR ARGUMENTS
+#
+parser.add_argument("--nx",  dest="nx",  action="store",type=int,default=12,  help="nbin 1st VAR",                    metavar="INTEGER")
+parser.add_argument("--ny",  dest="ny",  action="store",type=int,default=12,  help="nbin other VAR",                  metavar="INTEGER")
+parser.add_argument("--col1",dest="col1",action="store",type=int,default=1,   help="first column to read",            metavar="INTEGER")
+parser.add_argument("--col2",dest="col2",action="store",type=int,default=-1,help="last column to read",             metavar="INTEGER")
+parser.add_argument("--fr1", dest="fr1", action="store",type=int,default=1,   help="first line to read",              metavar="INTEGER")
+parser.add_argument("--fr2", dest="fr2", action="store",type=int,default=-1,help="last line to read",               metavar="INTEGER")
+parser.add_argument("--skip",dest="skip",action="store",type=int,default=1,   help="skip every this number of frames",metavar="INTEGER")
+parser.add_argument("-t","--transfer",dest="transfer",action="store_true",default=False,help="Toggle calculation of Transfer Entropy")
+options = parser.parse_args()
+
+save_objects = []
+out_array    = {}
+
+nbin_x   = options.nx
+nbin_y   = options.ny
+
+print "Reading File..."
+
+mol = gro.read_gro(options.file_gro)
+hbonds  = hb.hbonds(mol=mol,file_xpm=options.file_xpm,log=options.file_log)
+DATA = np.transpose(hbonds.nr_xpm.array)
+
+if options.col1 < 1 :
+        print " You must select a positive starting column (--col1 : {0:d})".format(options.col1)
+        exit(2)
+if options.col2 != -1:
+        if options.col2 < options.col1:
+                print " Last colum (--col2 : {1:4d}) must be higher than first (--col1 {0:4d})".format(options.col1,options.col2)
+                exit(2)
+        if options.col2 > (ncol - 1) :
+                print " Last colum (--col2 : {0:4d})  will be lowered to the actual number of column in the file ({1:4d}".format(options.col2,ncol-1)
+                options.col2 = ncol - 1
+if options.fr1 < 0:
+        options.fr1 = 0
+if options.fr2 != -1:
+        if options.fr2 <= options.fr1:
+                print " Last colum (--fr2 : {1:6d}) must be higher than first (--fr1 {0:6d})".format(options.fr1,options.fr2)
+                exit(2)
+if options.skip < 1:
+        print " You must select a positive skip tome (--skip : {0:4d})".format(options.skip)
+        exit(2)
+
+
+DATA = DATA[options.col1:options.col2,options.fr1:options.fr2:options.skip]
+
+nframes, ncol = DATA.shape
+
+hb_ts = ts(data=DATA,n_data=nframes,dim=1,nbins=2,bins=[np.array([0.0, 0.5, 1.0])])
+
+if options.file_mut != None:
+    M = np.loadtxt(options.file_mut)
+else:
+    M,E,P = hb_ts.mutual_info()
+
+if options.transfer :
+    T, [ [M_t,  E_t,  P_t ], [M_t1, E_t1, P_t1 ] ] = hb_ts.transfer_entropy(time=4)
+
+n_hb = M.shape[0]
+
+I = np.zeros(M.shape)
+M_o = np.zeros(M.shape)
+for i in np.arange(n_hb):
+    for j in np.arange(i+1,n_hb):
+        ref = M[i,i] * M[j,j]
+        M_o[i,j] = M[i,j] # I don't want to consider the diagonal term
+        if ref != 0:
+            I[i,j] = M[i,j] / ( M[i,i] + M[j,j] )
+
+percentile = 1. - ( 10. / ( n_hb ** 2 ) ) # Will show at least the top-5
+quantile_i = np.sort(np.ravel(I  ))[int(percentile*n_hb**2)-1] 
+quantile_m = np.sort(np.ravel(M_o))[int(percentile*n_hb**2)-1] 
+
+pi = np.where(I   >= quantile_i)
+pm = np.where(M_o >= quantile_m)
+
+print " Mutual Info {0:8.6f} percentile (val={1:10.6f})".format(percentile,quantile_m)
+print " Normal Info {0:8.6f} percentile (val={1:10.6f})".format(percentile,quantile_i)
+
+print "{0:>10s}{1:>30s}{2:>30s}".format("Mut.Info","HB1","HB2")
+for i in range(len(pm[0])):
+    if pm[0][i] != pm[1][i]:
+        print "{0:>10.6f}{1:>30s}{2:>30s}".format(M_o[pm[0][i],pm[1][i]], str(hbonds.hblist[pm[0][i]])[:30], str(hbonds.hblist[pm[1][i]])[:30])
+print "{0:>10s}{1:>30s}{2:>30s}".format("Norm.Info","HB1","HB2")
+for i in range(len(pi[0])):
+    if pi[0][i] != pi[1][i] :
+        print "{0:>10.6f}{1:>30s}{2:>30s}".format(I[pi[0][i],pi[1][i]], str(hbonds.hblist[pi[0][i]])[:30], str(hbonds.hblist[pi[1][i]])[:30]  )
+
+
+
+
+if options.file_out != None: 
+    if options.file_mut != None :
+        out_array["Normalized Mutual Info"] = I.tolist()
+        
+    else :
+        out_array["Mutual Info"]         = M.tolist()
+        out_array["Joint Entropies"]     = E.tolist()
+        out_array["Joint Probabilities"] = P.tolist()
+    if options.transfer:
+        out_array["Transfer Entropy"] = T.tolist() 
+
+
+for i in out_array.keys():
+    print " Out : {0:s}".format(i)
+
+save_objects.append(out_array)
+
+if save_objects != []:
+    with gzip.GzipFile(options.file_out, 'w') as outfile:
+        for obj in save_objects:
+            outfile.write(json.dumps(obj) + '\n')
+
+
+
+# reading
+#with gzip.GzipFile(options.file_in, 'r') as isfile:
+#    for line in infile:
+#        obj = json.loads(line)
+#
+
+
---
+++ ts/__init__.py
@@ -0,0 +1,241 @@
+import numpy as np
+import math as m
+import scipy.special as sp
+import copy
+
+
+
+class ts:
+        def __init__(self,data,n_data,dim,nbins=12,bins=None,prob=None,reshape=True,frame_row=True,dtype=float):
+                self.n_data = int(n_data)
+                self.dtype  = dtype
+                self.data   = np.array(data,dtype=dtype)
+                self.dim    = int(dim)
+                self.rep    = self._calc_rep()
+                self.nbins  = np.ones(dim,dtype=int)*nbins
+                self.min    = np.min(data)
+                self.max    = np.max(data)
+                self.bins   = bins # IT MUST BE A *LIST* OF *1-DIM ARRAYS*
+                self.reshape= reshape
+                self.prod=np.ones(dim+1,dtype=int)
+                self.frame_row=frame_row
+                self._shape(force=self.reshape)
+                self.prob=prob
+                if self.prob == None:
+                        self.prob_av=False
+                else:
+                        if self._check_prob_shape():
+                                self.prob_av=True
+                self.entropy=None
+                self.entropy_av=False
+        
+        def _calc_rep(self):
+                p=1
+                for i in range(len(self.data.shape)):
+                        p*=self.data.shape[i]
+                print " Found {0:4d} replica(s)".format(p / (self.n_data * self.dim))
+                return int(p / (self.n_data * self.dim))
+
+        def _shape(self,force=False):
+                #if self.reshape == False:
+                if  ( self._check_shape() & (not force) ) :
+                        print "Data already reshaped"
+                        return
+                self.reshape = False
+                proper_shape_data = np.zeros((self.rep,self.dim,self.n_data))
+                #row, lines = self.data.shape
+                if ( (self.dim == 1)  & (self.rep == 1) ):
+                        for k in np.arange(self.n_data):
+                                # IN_DATA [ N_sample, N_rep ]
+                                # DATA [ N_rep, DIM, N_sample ]
+                                proper_shape_data [0,0,k] = self.data[ k ]
+                else:
+                        if self.frame_row:
+                                # IN_DATA [ N_sample, N_rep ]
+                                for i in np.arange(self.rep):
+                                        for j in np.arange(self.dim):
+                                                for k in np.arange(self.n_data):
+                                                        # OUT_DATA [ N_rep, DIM, N_sample ]
+                                                        proper_shape_data [i,j,k] = self.data[ k , i * self.dim + j ]
+                        else:
+                                # IN_DATA [ N_rep, N_sample ]
+                                for i in np.arange(self.rep):
+                                        for j in np.arange(self.dim):
+                                                for k in np.arange(self.n_data):
+                                                        # OUT_DATA [ N_rep, DIM, N_sample ]
+                                                        proper_shape_data [i,j,k] = self.data[ i * self.dim + j, k ]
+                self.data = proper_shape_data
+                return
+
+        def _check_shape(self):
+                return ( self.data.shape == ( self.rep, self.dim, self.n_data ) )
+                
+        def _check_prob_shape(self):
+                return ( self.prob.shape == tuple([self.rep]) + tuple( self.nbins ) )
+        
+        def calc_prob(self):
+                if self.reshape == True:
+                        self._proper_shape()
+                # PROB ( N_rep, DIM_1, DIM_2 ... )
+                self.prob = np.zeros( np.hstack((self.rep, self.nbins)) )
+                for i in np.arange(self.rep):
+                        DATA = self.data[i]
+                        if self.dim == 1:
+                                if self.bins == None:
+                                        histo , self.bins = np.histogram(DATA,bins=self.nbins)
+                                else:
+                                        histo , null = np.histogram(DATA,bins=self.bins[0])
+                                self.prob[i] = 1.*histo / self.n_data
+                        else:
+                                if self.bins == None:
+                                        histo , self.bins = np.histogramdd(np.transpose(DATA),bins=self.nbins)
+                                else:
+                                        histo , null = np.histogramdd(np.transpose(DATA),bins=self.bins)
+                                self.prob[i] = 1.*histo / self.n_data
+                self.prob_av=True
+                return
+        
+        def calc_entropy(self,force=False):
+                if ( self.entropy_av & (not force) ):
+                        print "Entropy already available"
+                        return
+                if not self.prob_av :
+                        self.calc_prob()
+                self.entropy = np.zeros(self.rep)
+                for i in np.arange(self.rep):
+                        self.entropy[i] = -np.sum(sp.xlogy(self.prob[i],self.prob[i]))/m.log(2)
+                self.entropy_av=True
+                return
+        
+        def mutual_info(self):
+                if not self.entropy_av:
+                        self.calc_entropy()
+                P_joint = np.zeros( np.hstack( (self.rep, self.rep, self.nbins, self.nbins) ) )
+                E_joint = np.zeros((self.rep,self.rep))
+                M       = np.zeros((self.rep,self.rep))
+                total_step=self.rep*other.rep
+                for s in np.arange(self.rep):
+                        E_joint[s,s] = self.entropy[s] + self.entropy[s]
+                        M[s,s]       = E_joint[s,s]
+                        for o in np.arange(s+1,self.rep):
+                                print " Completed {0:6.3%}\r".format(float(s*o)/total_step)
+                                DATA = np.transpose(np.vstack((self.data[s],self.data[o])))
+                                histo , null = np.histogramdd(DATA,bins=self.bins+self.bins) # concatenate bins list with "+"
+                                P_joint[s,o] = 1.*histo / self.n_data
+                                E_joint[s,o] = -np.sum(sp.xlogy(P_joint[s,o],P_joint[s,o]))/m.log(2)
+                                E_joint[o,s] = E_joint[s,o]
+                                M[s,o] = self.entropy[s] + self.entropy[o] - E_joint[s,o]
+                                M[o,s] = M[s,o]
+                return M, E_joint, P_joint
+                
+        def mutual_info_other(self,other):
+                if self.n_data != other.n_data:
+                        print "The Number of Observation in the two time series are different ({0:d} != {0:d})".format(self.n_data,other.n_data)
+                        return None, None
+                if not self.entropy_av:
+                        self.calc_entropy()
+                if not other.entropy_av:
+                        other.calc_entropy()
+                P_joint = np.zeros( np.hstack( (self.rep, other.rep, self.nbins, other.nbins) ) )
+                E_joint = np.zeros((self.rep,other.rep))
+                M       = np.zeros((self.rep,other.rep))
+                total_step=self.rep*other.rep
+                for s in np.arange(self.rep):
+                        for o in np.arange(other.rep):
+                                # DATA* -> Transposed[ Self.DIM+Other.DIM, N_sample ]
+                                print " Completed {0:6.3%}\r".format(float(s*o)/total_step)
+                                DATA = np.transpose(np.vstack((self.data[s],other.data[o])))
+                                histo , null = np.histogramdd(DATA,bins=self.bins+other.bins) # concatenate bins list with "+"
+                                P_joint[s,o] = 1.*histo / self.n_data
+                                E_joint[s,o] = -np.sum(sp.xlogy(P_joint[s,o],P_joint[s,o]))
+                                M[s,o] = self.entropy[s] + other.entropy[o] - E_joint[s,o]
+                return M, E_joint, P_joint
+        
+        def digitized(self):
+                other = copy.deepcopy(self)
+                values = np.zeros((self.dim,self.nbins[0]))
+                for r in np.arange(self.rep):
+                        for d in np.arange(self.dim):
+                                for k in np.arange(self.n_data):
+                                        other.data[r,d,k] = np.max(np.where( self.bins[d] < self.data[r,d,k] ))
+                return other
+        
+        def traj(self,time,nbins):
+                prod_t = np.ones(time+1,dtype=int)
+                for i in np.arange(1,self.dim+1):
+                        self.prod[i] = self.prod[i-1] * self.nbins[i-1]
+                for i in np.arange(1,time+1):
+                                prod_t[i] =  prod_t[i-1] * self.prod[self.dim]
+                otherk_nbins  = nbins
+                otherk1_nbins = nbins * ( self.nbins ** self.dim )
+                other_nd    = self.n_data-time
+                other_k     = ts(np.zeros((self.rep,1,other_nd)),other_nd,1,otherk_nbins ,bins=None,prob=None,reshape=False,dtype=int)
+                other_k1    = ts(np.zeros((self.rep,1,other_nd)),other_nd,1,otherk1_nbins,bins=None,prob=None,reshape=False,dtype=int)
+                for r in np.arange(self.rep):
+                        for k in np.arange(self.n_data-time):
+                                hash_num = 0
+                                for t in np.arange(time):
+                                        #
+                                        # IN THIS IMPLEMENTATION THE MOST SIGNIFICATIVE "DIGIT" OF THE HASH_NUMBER
+                                        # IS DETERMINED BY THE LAST (IN TIME) VALUE OF TIME SERIE. THIS REQUIRE
+                                        # THAT THE NUMBER OF BINS IS DIFFERENT FOR THE O(K) AND O(K+1) PROBABILITY
+                                        # ( CHECK IF TRUE ) 
+                                        hash_num = hash_num + int(np.vdot(self.data[r,:,k+t],self.prod[:-1])) * prod_t[t]
+                                hash_num1 = hash_num + int(np.vdot(self.data[r,:,k+time],self.prod[:-1])) * prod_t[time]
+                                other_k.data[r,0,k]  = hash_num
+                                other_k1.data[r,0,k] = hash_num1
+                return other_k, other_k1
+                
+        
+        def transfer_entropy(self,time=2,nbins=None):
+                # CALCULATE THE TRANSFER ENTROPIES BETWEEN THE REPLICAS
+                if nbins == None:
+                         #
+                         # THIS IS THE OPTIMAL NUMBER OF BINS THAT SHOULD BE USED 
+                         # TO RESOLVE ALL THE POSSIBLE TIME SERIES (TO BE CHECKED)
+                         #
+                         # SINCE THE CALCULATION INVOLE THE CALCULATIO OF A JOINT
+                         # PROBABILITY A MATRIX OF DIM nbins*(nbins*(nbin^dim)) IS CREATED:
+                         # BE CAREFUL SINCE THIS WILL QUICKLY SATURATE THE MEMORY!!
+                         #
+                         # EXAMPLE:
+                         #  nbin =10; dim =1 ; time =2 ->  nbins = 10^2 
+                         #                              -> nbins*nbins*nbin = 10^5 
+                         #
+                         #  nbin =10, dim =1 ; time =3 -> nbins = 10^3
+                         #                             -> nbins*nbins*nbin = 10^7 !!!!
+                         # 
+                         #
+                        nbins= int(self.nbins) ** int( self.dim * time )
+                ok, ok1 = self.traj(time,nbins)
+                min_h , max_h  = [ int(round(np.min(ok.data ))), int(round(np.max(ok.data ))) ]
+                min_h1, max_h1 = [ int(round(np.min(ok1.data))), int(round(np.max(ok1.data))) ]
+                print " traj(K)   : {0:6d}-{1:6d}; nbins:{2:6d}".format(min_h, max_h, ok.nbins[0])
+                print " traj(K+1) : {0:6d}-{1:6d}; nbins:{2:6d}".format(min_h1,max_h1,ok1.nbins[0])
+                ok.bins  = [np.linspace(min_h, max_h, ok.nbins+1)]
+                ok1.bins = [np.linspace(min_h1,max_h1,ok1.nbins+1)]
+                print "Calculating Mutual Info of traj(K)s"
+                M,  E,  P  = ok.mutual_info()
+                print "Calculating Mutual Info of traj(K+1)s with respect of traj(K)s"
+                # WHICH ONE IS CORRECT ?!
+                M1, E1, P1 = ok.mutual_info_other(ok1)
+                #M1, E1, P1 = ok1.mutual_info_other(ok)
+                print "Done"
+                T = np.zeros((self.rep,self.rep))
+                # T(I,J) = H(I) - H(J) - H(I_k+1, J_k) + H(I_k,J_k)
+                print "Calculating Transfer Entropy"
+                for s in np.arange(self.rep):
+                        for o in np.arange(self.rep):
+                                T[s,o] = ok.entropy[s] + ok.entropy[o] - M1[s,o] + M[s,o]
+                return T, [ [M,  E,  P ], [M1, E1, P1 ] ] 
+                
+                
+                                        
+                                        
+                
+                
+                
+                                        
+                                        
+                        
+                
