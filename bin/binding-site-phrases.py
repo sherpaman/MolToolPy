79e707ab22011e139a29a674eab07c347f8be4c6
Andrea Coletta Local <coletta@kemtrp.chem.au.dk>
2016-11-11 13:01:59 +0100

    added bindin-site phrases script

---
+++ bin/binding-site-phrases.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import phrases
+from scipy import stats
+from math import *
+import MDAnalysis as MD
+from argparse import ArgumentParser
+parser = ArgumentParser( description = 'Find Binding Sites in Receptor+Ligand MD simulation')
+#
+# INPUT FILES
+#
+parser.add_argument("-f","--traj",dest="traj",action="store",type=str,default=None,help="Input Trajectory",required=True,metavar="TRAJ FILE")
+parser.add_argument("-t","--top",dest="top",action="store",type=str,default=None,help="Input Topology",required=True,metavar="TOPOL FILE")
+#
+# OUTPUT FILES
+#
+parser.add_argument("-o","--out",dest="out",action="store",type=str,default=None,required=True,help="Output File Name",metavar="DAT FILE")
+#
+# VAR ARGUMENTS
+#
+parser.add_argument("-c","--cutoff",dest="cutoff",action="store",type=float,default=0.0,help="toggle auto-saving matrix plot")
+parser.add_argument("-b","--begin",dest="begin",action="store",type=int,default=0,help="First frame to read")
+parser.add_argument("-e","--end",dest="end",action="store",type=int,default=-1,help="Last frame to read")
+parser.add_argument("-s","--skip",dest="skip",action="store",type=int,default=1,help="number of frame to skip", metavar="INTEGER")
+#parser.add_argument("-p","--plot",dest="plot",action="store_true",default=False,help="toggle auto-saving matrix plot")
+parser.add_argument("-r","--receptor",dest="receptor",action="store",type=str,default="Protein",help="Selection string for the Receptor")
+parser.add_argument("-l","--ligand",dest="ligand",action="store",type=str,default="not Protein",help="Selection strin for the Ligand")
+#
+options = parser.parse_args()
+
+
+top = options.top
+trj = options.traj
+b    = options.begin
+e    = options.end
+skip = options.skip
+cutoff = options.cutoff
+rec_str = options.receptor
+lig_str = options.ligand
+
+min_phrase_len = 3
+threshold = 0.2  5
+lig_dist_cutoff = 6.0
+
+u = MD.Universe(top,trj)
+
+receptor = u.select_atoms("protein")
+ligand = u.select_atoms("resname SDS")
+
+P = phrases.phrases(u,receptor,ligand,lig_dist_cutoff,min_phrase_len)
+
+P.find_phrases(b,e,skip)
+
+
+with open(options.out+'-phrases.dat', 'wb') as output:
+    pickle.dump(P, output, pickle.HIGHEST_PROTOCOL)
+
+P.calc_dist()
+
+np.savez(options.out+"-distance.npz",P.D)
+
+print("using Cut-Off : {0:10.6f}".format(cutoff))
+
+P.find_cluster(cutoff)
+P.cluster_phrases()
+P.life_time()
+
+perc=np.sum(P.p_cl>0,axis=0)/float(len(P.p_cl))
+life_time=np.zeros(int(max(P.labels)+1))
+
+#print P.LT
+with open(options.out+"-binding-site.dat","w") as f:
+    for i in range(1,int(max(P.labels)+1)):
+        life_time[i] = np.average(P.LT[i])*P.dt
+        clusters=''
+        for e in P.clusters[i]:
+            clusters=clusters+' {0:3s}'.format(e)
+        f.write("{0:3d}|{1:4d} : ({2:s}) | perc: {3:6.4f} | life-time: {4:10.6f} ns\n".format(i,P.centroid[i],clusters,perc[i],life_time[i]/1000.0))
+
+
+quit()
--- phrases/__init__.py
+++ phrases/__init__.py
@@ -1,33 +1,92 @@
+import numpy as np
+import pickle
 
-class phrases(universe,receptor,ligand,cutoff,min_len):
-    self.universe = universe
-    self.receptor = receptor
-    self.ligand = ligand
-    self.cutoff = cutoff
-    self.min_len = min_len
-    self.phrases = []
-    self.D = None
+def read_phrases(f_dat):
+    with open(f_dat) as f:
+        L=pickle.load(f)
+        out = phrases()
+        out.phrases = L
+    return out
+
+def _mj(a,c,t): 
+    list_j = [ float(len(set(a) & set(i))) /  float(len(set(a) | set(i))) for i in c ] # Jaccad Similarity
+    if max(list_j) < t :
+        return 0
+    else:
+        return np.argmax(list_j)
+
+def _gromos(D,cutoff,min_sz):
+    M=np.copy(D)
+    cent=[len(M)]
+    elem = np.zeros(len(M))
+    idx  = np.arange(len(M))
+    cl_s = min_sz 
+    n_cl = 1
+    while (cl_s >= min_sz):
+        loc_elem = np.zeros(len(M))
+        nngb  = np.sum(M<cutoff, axis=1)
+        if len(nngb) < min_sz :
+            break
+        loc_cent = np.argmax(nngb)
+        clust = np.where(M[loc_cent]<cutoff)[0]
+        cent.append(idx[loc_cent])
+        cl_s = len(clust)
+        elem[idx[clust]] = n_cl
+        loc_elem[clust] = n_cl
+        M=M[loc_elem==0][:,loc_elem==0]
+        idx = idx[loc_elem==0]  
+        n_cl += 1
+    return elem,cent
+
+    
+def _calc_lifetime(data):
+    n_fr , n_cl = data.shape
+    LT = []
+    for n in np.arange(n_cl):
+        d0 = np.copy(data[:,n])
+        m = np.max(d0)
+        loc_t=[]
+        for i in range(int(m)-1):
+            d = (np.concatenate([[0],d0,[0]])>0).astype(int)
+            up   = np.array([ i   for i in np.arange(1,n_fr+1) if (d[i] - d[i-1] ==  1) ])
+            down = np.array([ i   for i in np.arange(2,n_fr+2) if (d[i] - d[i-1] == -1) ])
+            loc_t= np.concatenate([loc_t,down-up])
+            d0[d0!=0] -= 1
+        LT.append(loc_t)
+    return LT
+
+
+class phrases:
+    def __init__(self,universe=None,receptor=None,ligand=None,cutoff=None,min_len=None):
+        self.universe = universe
+        self.receptor = receptor
+        self.ligand = ligand
+        self.cutoff = cutoff
+        self.min_len = min_len
+        self.phrases = []
+        self.D = None
 
     def find_phrases(self,b,e,skip):
         self.phrases = []
         print ("Start reading trajectory")
-        for fr in u.trajectory[b:e:skip]:
-            dt = fr.time - old_t
+        old_t=0.0
+        for fr in self.universe.trajectory[b:e:skip]:
+            self.dt = fr.time - old_t
             old_t = fr.time
             phrase=[]
             for lr in self.ligand.residues:
-                p = list(np.unique(self.receptor.select_atoms("around %f global group grp" %(cutoff), grp=lr).resids).astype(int))
-                if len(p) > min_phrase_len:
+                p = list(np.unique(self.receptor.select_atoms("around %f global group grp" %(self.cutoff), grp=lr).resids).astype(int))
+                if len(p) > self.min_len:
                     phrase.append(p)
             self.phrases.append(phrase)
         print("Done!")
 
-    def calc_dist(self)
+    def calc_dist(self):
         p_a = []
         self.nobs = 0
         for t in self.phrases:
             for f in t:
-                self.nobs = nobs + 1 
+                self.nobs = self.nobs + 1 
                 for a in f:
                     p_a.append(a)
         
@@ -40,69 +99,24 @@ class phrases(universe,receptor,ligand,cutoff,min_len):
                         C[f[i]-1,f[j]-1] += 1
                         C[f[j]-1,f[i]-1] += 1
         
-        self.D = - np.log2(C/nobs)
+        self.D = - np.log2(C/self.nobs)
 
-    def find_cluster(self):
-        e, c = self._gromos(self.D,cutoff,min_sz=self.min_len)
+    def find_cluster(self,cutoff):
+        e, c = _gromos(self.D,cutoff,self.min_len)
+        self.centroid = c
         self.labels   = e
         self.clusters = [ np.where(e==i)[0] for i in range(int(max(e)+1)) ]
 
-    def cluster_phrases(self,threshold=0.5):
-        self.phrases_cl = [ [ self._mj(p,clusters,threshold) for p in t ] for t in self.phrases]
+    def cluster_phrases(self,threshold=0.25):
+        self.phrases_cl = [ [ _mj(p,self.clusters,threshold) for p in t ] for t in self.phrases]
         n_fr = len(self.phrases_cl)
         n_cl = int(max(self.labels))
         self.p_cl = np.zeros((n_fr,n_cl+1))
         for i in range(n_fr):
             for j in self.phrases_cl[i]:
                 self.p_cl[i,j] += 1
-
-    def life_time(self):
-        self.life_time=self._calc_lifetime(self.p_cl)
-
-    def _mj(a,c,t): 
-        list_j = [ float(len(set(a) & set(i))) /  float(len(set(a) | set(i))) for i in c ] # Jaccad Similarity
-        if max(list_j) < t :
-            return 0
-        else:
-            return np.argmax(list_j)
-
-    def _gromos(D,cutoff,min_sz=self.min_len):
-        M=np.copy(D)
-        cent=[len(M)]
-        elem = np.zeros(len(M))
-        idx  = np.arange(len(M))
-        cl_s = min_sz 
-        n_cl = 1
-        while (cl_s >= min_sz):
-            loc_elem = np.zeros(len(M))
-            nngb  = np.sum(M<c, axis=1)
-            if len(nngb) < min_sz :
-                break
-            loc_cent = np.argmax(nngb)
-            clust = np.where(M[loc_cent]<c)[0]
-            cent.append(idx[loc_cent])
-            cl_s = len(clust)
-            elem[idx[clust]] = n_cl
-            loc_elem[clust] = n_cl
-            M=M[loc_elem==0][:,loc_elem==0]
-            idx = idx[loc_elem==0]  
-            n_cl += 1
-        return elem,cent
-
         
-    def _calc_lifetime(data):
-        n_fr , n_cl = data.shape
-        LT = []
-        for n in np.arange(n_cl):
-            d0 = np.copy(data[:,n])
-            m = np.max(d0)
-            loc_t=[]
-            for i in range(int(m)-1):
-                d = (np.concatenate([[0],d0,[0]])>0).astype(int)
-                up   = np.array([ i   for i in np.arange(1,n_fr+1) if (d[i] - d[i-1] ==  1) ])
-                down = np.array([ i   for i in np.arange(2,n_fr+2) if (d[i] - d[i-1] == -1) ])
-                loc_t= np.concatenate([loc_t,down-up])
-                d0[d0!=0] -= 1
-            LT.append(loc_t)
-        return LT
+    def life_time(self):
+        self.LT=_calc_lifetime(self.p_cl)
+
     
