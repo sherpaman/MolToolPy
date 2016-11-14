1a22f983932f7714c9cee0bc247ee790a4bc467a
Andrea Coletta Local <coletta@kemtrp.chem.au.dk>
2016-11-14 13:25:48 +0100

    syntax correction

--- bin/binding-site-phrases.py
+++ bin/binding-site-phrases.py
@@ -96,12 +96,9 @@ if cutoff==None:
     plt.plot(c_val[elbow:n_val-buf],i2[elbow]+c_val[elbow:n_val-buf]*s2[elbow])
     plt.savefig('{0:s}-elbow-point.png'.format(options.out),fmt="png")
 
-print("Using Cut-Off                 : {0:10.6f}".format(cutoff))
-cutoff_percentile = (p[np.min(np.where(perc>cutoff))]+p[np.max(np.where(perc<cutoff))]) / 2
-print("This value corresponds to the   {0:8.4f} percentile".format(cutoff_percentile))
 
 P.find_cluster(cutoff)
-P.cluster_phrases()
+P.cluster_phrases(threshold)
 P.life_time()
 
 perc=100.*np.sum(P.p_cl>0,axis=0)/float(len(P.p_cl))
@@ -109,12 +106,15 @@ life_time=np.zeros(int(max(P.labels)+1))
 
 #print P.LT
 with open(options.out+"-binding-site.dat","w") as f:
+    f.write("Using Cut-Off                 : {0:10.6f}".format(cutoff))
+    cutoff_percentile = (p[np.min(np.where(perc>cutoff))]+p[np.max(np.where(perc<cutoff))]) / 2
+    f.write("This value corresponds to the   {0:8.4f} percentile".format(cutoff_percentile))
     for i in range(1,int(max(P.labels)+1)):
         life_time[i] = np.average(P.LT[i])*P.dt
         clusters=''
         for e in P.clusters[i].astype(int):
             clusters=clusters+' {0:3s}'.format(str(e))
-        f.write("{0:3d}|{1:4d} : ({2:s}) | perc: {3:6.4f} | life-time: {4:8.3f} ns\n".format(i,P.centroid[i],clusters,perc[i],life_time[i]/1000.0))
+        f.write("{0:3d}|{1:4d} : ({2:s}) | perc: {3:6.4f} | life-time: {4:8.3f} ns\n".format(i,P.centroid[i]+1,clusters,perc[i],life_time[i]/1000.0))
 
 
 quit()
